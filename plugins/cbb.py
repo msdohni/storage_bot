120e7dbf4debef9f266258abdc93bf9cd77c2dbc
msdohni <141110310+msdohni@users.noreply.github.com>
2024-01-14 23:01:05 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Master : <a href='https://t.me/STThamizhan'>Click here</a>\n○ Channel : <a href='https://t.me/+nSD05P6dHWZmZTc1'>\n○ Support Group : <a href='https://t.me/+YUFEObApJqIwMzBl'></b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Master : <a href='https://t.me/STThamizhan'>Click here</a>\n○ Channel : <a href='https://t.me/+nSD05P6dHWZmZTc1'>click</a>\n○ Support Group : <a href='https://t.me/+YUFEObApJqIwMzBl'>click</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
